07c689231863c14f9cfce97c489cd47b262cf2c0
Zakorree <shayazachj@gmail.com>
2026-06-25 16:35:27 -0700

    Initial commit - Day 1 exercises

---
+++ Excercise3.py
@@ -0,0 +1,13 @@
+#examples of different python data types
+
+print(5) # Integer
+print (9.5) # Float
+print(4 - 4j) #Complex
+print("Soup") # String
+print(True) # Boolean
+print(['Asabeneh', 'Python', 'Finland'])# List
+print(('Asabeneh', 'Python', 'Finland')) # Tuple
+print({'Asabeneh', 'Python', 'Finland'}) # Set
+print({'Asabeneh': 'dog', 'Python': 'dog', 'Finland': 'dog'}) # Dictionary
+# Distance bewteen (2, 3) and (10, 8)
+print(((10-2) ** 2) + ((8-3) ** 2) ** (1 / 2))
